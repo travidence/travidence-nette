5e5aede2845d30d191c13dbe1b05cc1e7f4d6cf2
Štěpán Ševčík <Thoronir42@Gmail.com>
2018-10-17 13:44:30 +0200

    Fix router namespace

--- src/Model/UserManager.php
+++ src/Model/UserManager.php
@@ -22,13 +22,10 @@ final class UserManager implements Nette\Security\IAuthenticator
 		COLUMN_ROLE = 'role';
 
 
-	/** @var Nette\Database\Context */
-	private $database;
 
-
-	public function __construct(Nette\Database\Context $database)
+	public function __construct()
 	{
-		$this->database = $database;
+
 	}
 
 
--- src/Router/RouterFactory.php
+++ src/Router/RouterFactory.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace App\Router;
+namespace Travidence\Router;
 
 use Nette;
 use Nette\Application\Routers\Route;
